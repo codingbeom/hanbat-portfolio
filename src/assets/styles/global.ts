842358c36f225b74ec6ee36d5a1b69a6ea2b5e47
leekibeom <lkb951103@gmail.com>
2024-03-28 17:14:50 +0900

    기본 구조

---
+++ src/assets/styles/global.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+
+const styled = { createGlobalStyle };
+
+export const GlobalStyleProvider = styled.createGlobalStyle`
+	*,
+	*::after,
+	*::before {
+		padding: 0;
+		margin: 0;
+		box-sizing: border-box;
+		touch-action: pan-y;
+	}
+
+	:root {
+		font-family: "Pretendard";
+		line-height: 1.5;
+		font-synthesis: none;
+		text-rendering: optimizeLegibility;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+	}
+`;
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "/hanbat-portfolio/",
+	base: "/hanbat-portfolio",
+	resolve: {
+		alias: [
+			{ find: "@", replacement: "/src" },
+			{ find: "&", replacement: "/src/assets/images" },
+		],
+	},
 });
